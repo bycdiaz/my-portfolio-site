4cec0bb46504ad869512fa8453f705c9a5f0c79d
Carlos Diaz <cdiaz86@gmail.com>
2020-07-18 12:32:43 -0400

    Changes background color to alabaster.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Routes from "./components/Routes";
 const GlobalStyle = createGlobalStyle`
   body {
-    background-color: #FEFEFA;
+    background-color: #EDEAE0;
   }
 `
 
