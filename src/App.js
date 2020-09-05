dc0a89146e91ce936e5e0c03f8a5c0fed3adbf5d
Carlos Diaz <cdiaz86@gmail.com>
2020-09-05 16:53:44 -0400

    Removes global styles component.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components'
 
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-    background-color: #EDEAE0;
-  }
-`
-
 function App() {
   return (
     <div>
-      <GlobalStyle />
       <Header/>
       <Home />
       <Skills />
