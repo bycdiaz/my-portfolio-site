734e1540aea90f24663c07f16b0fc63ec19ca927
Carlos Diaz <cdiaz86@gmail.com>
2020-09-05 21:03:31 -0400

    Adds section dividers.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import Header from "./components/Header";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+
+const SectionDivider = styled.hr`
+  border-top: 2px solid #e35a00;
+  color: #e35a00;
+  margin: 50px auto;
+  max-width: 90%;
+`
 
 function App() {
   return (
@@ -10,7 +19,9 @@ function App() {
       <Header className="header"/>
       <div className="app-content">
         <About />
+        <SectionDivider />
         <Skills />
+        <SectionDivider />
         <Projects />
       </div>
     </>
