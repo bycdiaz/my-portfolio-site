8c976a83b328ff3d0d4b28fdec19dfa4d47b3d20
Carlos Diaz <cdiaz86@gmail.com>
2020-11-15 13:30:51 -0500

    Adds hp screenshot.

--- src/json/projectList.js
+++ src/json/projectList.js
@@ -1,8 +1,18 @@
+import hp from "../images/hp-landing.png";
 import configzmeImage from "../images/configzme.png";
 import prereqCheck from "../images/prereqcheck.png";
 import grotonToggle from "../images/grotonfd-toggle.gif";
 
 const projectList = [
+  {
+    projectName: 'Hyper Protocol',
+    description: 'Transformed paper emergency protocol manual into progressive web application that equips firefighters, EMTs, and paramedics with the ability to search and review emergency protocols on mobile devices while responding to emergencies, even when offline. Developed admin interface to manage users and create, update and delete emergency protocols.',
+    // repoLink: 'https://github.com/bycdiaz/configzme/tree/master/frontend',
+    // liveLink: 'https://configz.me/',
+    madeWith: 'Made with Node/Express, React, styled-components, Indexed DB (Dexie), Mongodb, and sockets.',
+    demo: 'Demo available upon request.',
+    image: hp,
+  },
   {
     projectName: 'Configz.me',
     description: 'Developed front-end for configuration files management tool that allows users to upload new files, edit files, view file contents, and delete files.',
